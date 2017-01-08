d615cdefd6fef550c33e774c58e6514d978c5916
Kaigokan <take.tkt@gmail.com>
2017-01-08 12:54:49 +0900

    Add random record pick method to Theme

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -2,4 +2,8 @@ class Theme < ApplicationRecord
   validates :title, presence: true,
                     uniqueness: true,
                     length: {maximum: 255}
+
+  def self.sample
+    find(Theme.pluck(:id).sample)
+  end
 end
--- spec/models/theme_spec.rb
+++ spec/models/theme_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe Theme, type: :model do
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_uniqueness_of(:title) }
   it { is_expected.to validate_length_of(:title).is_at_most(255) }
+
+  describe "#self.sample" do
+    before { 2.times { |i| Theme.create(:title => "theme#{i}") } }
+    subject { Theme.sample }
+    it { expect(subject).to be_an_instance_of(Theme) }
+
+    context "record does not exist" do
+      before { Theme.destroy_all }
+      it { expect{subject}.to raise_error(ActiveRecord::RecordNotFound) }
+    end
+  end
 end
